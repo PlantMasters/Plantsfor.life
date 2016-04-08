0f13ba2526a70ce317e8c796b418586656cc9aff
Jessica Morrell <jessica.ann.morrell@gmail.com>
2016-04-08 12:12:23 -0600

    gulping

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ var concat = require('gulp-concat');
 var uglifycss = require('gulp-uglifycss');
 var ngAnnotate = require('gulp-ng-annotate');
 var server = require('gulp-webserver');
-var htmlmin = require('gulp-htmlmin');
+//var htmlmin = require('gulp-htmlmin');
 
 
-var watcher = gulp.watch(['./main/js/**/**/*.js', './main/styles/*.scss', './main/views/**/**/*.html', './main/*.html'], ['default']); 
+var watcher = gulp.watch(['./main/js/**/*.js', './main/styles/*.scss', './main/views/**/**/*.html', './main/*.html'], ['default']); 
 watcher.on('change', function( event ) {
         console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
 });
@@ -17,24 +17,24 @@ gulp.task('sass', function() {
     gulp.src('./main/styles/*.scss')
         .pipe(sass())
         .pipe(uglifycss())
-        //.pipe(concat('styles.css')) 
+        .pipe(concat('styles.css')) 
         .pipe(gulp.dest('./public/styles'))
 });
 gulp.task('javascript', function() {
-    gulp.src('./main/js/**/**/*.js')
-        .pipe(ngAnnotate())
+    gulp.src('./main/js/**/*.js')
+        //.pipe(ngAnnotate())
         // .pipe(uglify())
         .pipe(concat('all.min.js'))
         .pipe(gulp.dest('./public/scripts'))
 });
 gulp.task('html', function() {
     gulp.src('./main/views/**/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
+        // .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./public/views'))
 });
 gulp.task('index', function() {
     gulp.src('./main/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
+        // .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./public'))
 });
 gulp.task('default', ['sass', 'javascript', 'html', 'index']);

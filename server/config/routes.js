4aba13b6210b3affbcf44da62178fd497a758e66
David Andelin <iandelin@outlook.com>
2016-04-12 14:07:13 -0600

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,28 @@
+var auth = require('./auth.js'),
+  users = require('./users'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
+
+module.exports = function(app) {
+
+  app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
+  app.post('/api/users', users.createUser);
+  app.put('/api/users', users.updateUser);
+
+  app.post('/login', auth.authenticate);
+
+  app.post('/logout', function(req, res){
+    req.logout();
+    res.end();
+  });
+
+  app.all('/api/*', function(req, res) {
+    res.send(404);
+  });
+
+  app.get('*', function(req, res, next) {
+    res.render('index', {
+      bootstrappedUser: req.user
+    });
+  });
+};

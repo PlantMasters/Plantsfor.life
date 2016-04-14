f4a2004daaa853f47646d6133d3748d9e257dc7a
Jessica Hathaway <hathaway.jess@gmail.com>
2016-04-14 16:53:59 -0600

    add to finalArray

--- main/js/controllers/mainCtrl.js
+++ main/js/controllers/mainCtrl.js
@@ -10,8 +10,8 @@ angular.module('plantMasters').controller('mainCtrl', function($scope, $window, 
 
    $scope.medicalUses = ["Alternative Medicine", "Bacteria/Viruses/Fungi/Parasites", "Bites/Stings", "Bones", "Bowels/Bladder",
  "Cancer", "Chest/Lungs", "Ear/Nose/Throat/Eyes", "Fevers", "General Well Being", "Heart/Blood", "Infectious Diseases", "Liver/Kidneys", "Nerves/Muscles", "Other", "Pain Relief", "Sex/Reproduction", "Skin/Hands/Feet", "Stomach", "Wounds/Bruises", "Whole Body"];
-   $scope.edibleUses = ["Chocolate", "Coffee", "Coloring", "Condiment", "Curdling", "Drink", "Egg", "Gelatine", "Gum", "Milk", "Oil", "Pectin", "Rutin", "Salt", "Stabilizer", "Sweetener", "Tea"];
    $scope.otherUses = ["Building", "Clothing", "Dyes/Paints/Ink/Paper", "Fertilizers", "Fire/Lighting", "Bathroom", "Garden", "Home", "Kitchen", "Other", "Pesticides", "Woodwork/Crafts"];
+   $scope.edibleUses = ["Chocolate", "Coffee", "Coloring", "Condiment", "Curdling", "Drink", "Egg", "Gelatine", "Gum", "Milk", "Oil", "Pectin", "Rutin", "Salt", "Stabilizer", "Sweetener", "Tea"];
    $scope.zones = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
    $scope.typedUse;
    $scope.searchInputUse = function() {
--- main/js/services/mainSearchService.js
+++ main/js/services/mainSearchService.js
@@ -14,18 +14,21 @@ angular.module('plantMasters').service('mainSearchService', function($http, $q, 
         }
         //invoke function here
     };
-    this.medicalSelected = ["Alternative Medicine"];
+    this.medicalSelected = [];
     this.manageMedicalSelected = function(use) {
         var found = false;
         for (var i = 0; i < this.medicalSelected.length; i++) {
             if (this.medicalSelected[i] === use) {
                 found = true;
+                this.removeMedicalSpecific(use);
                 this.medicalSelected.splice(i, 1);
             }
         }
         if (!found) {
+            this.addMedicalSpecific();
             this.medicalSelected.push(use);
         }
+          console.log('Line 31 ' + this.medicalSelected);
     };
     this.edibleSelected = [];
     this.manageEdibleSelect = function(use) {
@@ -61,216 +64,295 @@ angular.module('plantMasters').service('mainSearchService', function($http, $q, 
 //        })
 //    }
 
-this.alternativeMed = ['Alternative', 'Aromatherapy', 'Bach', 'Homeopathy'];
-​
-this.bacteria = ['Anthelmintic', 'Antibacterial', 'Antibiotic', 'Antifungal', 'Antiviral', 'Parasiticide', 'Vermifuge', 'Warts'];
-​
-this.bites = ['Antidote', 'Stings'];
-​
-this.bones = ['Antiarthritic', 'Antiinflammatory', 'Antirheumatic'];
-​
-this.bowels = ['Antihaemorrhoidal', 'Aperient', 'Carminative', 'Cathartic', 'Hydrogogue', 'Laxative', 'Purgative'];
-​
-this.cancer = ['Antitumor', 'Cancer', 'Cytostatic', 'Cytotoxic', 'Resolvent'];
-​
-this.chest = ['Antiasthmatic', 'Antitussive', 'Decongestant', 'Demulcent', 'Expectorant', 'Pectoral'];
-​
-this.ear = ['Errhine', 'Mouthwash', 'Mydriatic', 'Odontalgic', 'Ophthalmic', 'Sialagogue', 'Sternutatory'];
-​
-this.fevers = ['Antipyretic', 'Febrifuge'];
-​
-this.general = ['Antiscorbutic', 'Balsamic', 'Nutritive', 'Tonic'];
-​
-this.heart = ['Anticholesterolemic', 'Anticoagulant', 'Blood purifier', 'Blood tonic', 'Cardiac', 'Cardiotonic', 'Haemolytic', 'Hypoglycaemic', 'Hypotensive', 'Vasoconstrictor', 'Vasodilator'];
-​
-this.infectious = ['Antiperiodic', 'Antiscrophulatic' , 'TB', 'VD'];
-​
-this.liver = ['Antibilious', 'Cholagogue', 'Diuretic', 'Haemostatic', 'Hepatic', 'Kidney', 'Lithontripic'];
-​
-this.nerves = ['Antispasmodic', 'Nervine'];
-​
-this.otherMed = ['Antidandruff', 'Antihydrotic', 'Antiphlogistic', 'Antivinous', 'Appetizer', 'Aromatic', 'Astringent', 'Bitter', 'Deodorant', 'Diaphoretic', 'Emollient', 'Enuresis', 'Hallucinogenic', 'Hypnotic', 'Irritant', 'Miscellany', 'Refrigerant', 'Restorative', 'Rubefacient', 'Sedative', 'Stimulant'];
-​
-this.pain = ['Anaesthetic', 'Analgesic', 'Anodyne', 'Lenitive', 'Narcotic'];
-​
-this.sex = ['Abortifacient', 'Anaphrodisiac', 'Aphrodisiac', 'Birthing aid', 'Contraceptive', 'Emmenagogue', 'Galactofuge', 'Galactogogue', 'Infertility', 'Oxytoxic', 'Uterine tonic', "Women's complaints"];
-​
-this.skin = ['Acrid', 'Antidermatosic', 'Antipruritic', 'Foot care', 'Skin', 'Vesicant'];
-​
-this.stomach = ['Antacid', 'Antiemetic', 'Digestive', 'Emetic', 'Stomachic'];
-​
-this.wounds = ['Antiecchymotic', 'Antiseptic', 'Detergent', 'Disinfectant', 'Plaster', 'Poultice', 'Salve', 'Styptic', 'Vulnerary'];
-​
-this.wholeBody = ['Adaptogen', 'Deobstruent', 'Depurative'];
 
 
 //loop through medicalSelected
+
+//WRAP THEEEES IN A function
+//call that function after array is updated in manage____Selected function
+//call another function to send get with finalSlectedArray
+
 this.finalSelectedArray = [];
 
+this.addMedicalSpecific = function() {
+
+  var alternativeMed = ['Alternative', 'Aromatherapy', 'Bach', 'Homeopathy'];
+  var bacteria = ['Anthelmintic', 'Antibacterial', 'Antibiotic', 'Antifungal', 'Antiviral', 'Parasiticide', 'Vermifuge', 'Warts'];
+  var bites = ['Antidote', 'Stings'];
+  var bones = ['Antiarthritic', 'Antiinflammatory', 'Antirheumatic'];
+  var bowels = ['Antihaemorrhoidal', 'Aperient', 'Carminative', 'Cathartic', 'Hydrogogue', 'Laxative', 'Purgative'];
+  var cancer = ['Antitumor', 'Cancer', 'Cytostatic', 'Cytotoxic', 'Resolvent'];
+  var chest = ['Antiasthmatic', 'Antitussive', 'Decongestant', 'Demulcent', 'Expectorant', 'Pectoral'];
+  var ear = ['Errhine', 'Mouthwash', 'Mydriatic', 'Odontalgic', 'Ophthalmic', 'Sialagogue', 'Sternutatory'];
+  var fevers = ['Antipyretic', 'Febrifuge'];
+  var general = ['Antiscorbutic', 'Balsamic', 'Nutritive', 'Tonic'];
+  var heart = ['Anticholesterolemic', 'Anticoagulant', 'Blood purifier', 'Blood tonic', 'Cardiac', 'Cardiotonic', 'Haemolytic', 'Hypoglycaemic', 'Hypotensive', 'Vasoconstrictor', 'Vasodilator'];
+  var infectious = ['Antiperiodic', 'Antiscrophulatic' , 'TB', 'VD'];
+  var liver = ['Antibilious', 'Cholagogue', 'Diuretic', 'Haemostatic', 'Hepatic', 'Kidney', 'Lithontripic'];
+  var nerves = ['Antispasmodic', 'Nervine'];
+  var otherMed = ['Antidandruff', 'Antihydrotic', 'Antiphlogistic', 'Antivinous', 'Appetizer', 'Aromatic', 'Astringent', 'Bitter', 'Deodorant', 'Diaphoretic', 'Emollient', 'Enuresis', 'Hallucinogenic', 'Hypnotic', 'Irritant', 'Miscellany', 'Refrigerant', 'Restorative', 'Rubefacient',
+  'Sedative', 'Stimulant'];
+  var pain = ['Anaesthetic', 'Analgesic', 'Anodyne', 'Lenitive', 'Narcotic'];
+  var sex = ['Abortifacient', 'Anaphrodisiac', 'Aphrodisiac', 'Birthing aid', 'Contraceptive', 'Emmenagogue', 'Galactofuge', 'Galactogogue', 'Infertility', 'Oxytoxic', 'Uterine tonic', "Women's complaints"];
+  var skin = ['Acrid', 'Antidermatosic', 'Antipruritic', 'Foot care', 'Skin', 'Vesicant'];
+  var stomach = ['Antacid', 'Antiemetic', 'Digestive', 'Emetic', 'Stomachic'];
+  var wounds = ['Antiecchymotic', 'Antiseptic', 'Detergent', 'Disinfectant', 'Plaster', 'Poultice', 'Salve', 'Styptic', 'Vulnerary'];
+  var wholeBody = ['Adaptogen', 'Deobstruent', 'Depurative'];
+
 for (var i = 0; i < this.medicalSelected.length; i++) {
-  if (i === "Alternative Medicine") {
-    for (var i = 0; i < this.alternativeMed.length; i++) {
-      this.finalSelectedArray.push(i);
+  if (this.medicalSelected[i] === "Alternative Medicine") {
+    for (var j = 0; j < alternativeMed.length; j++) {
+      this.finalSelectedArray.push(alternativeMed[j]);
     }
-    //loop through AM array, push i to finalSelectedArray
   }
-  if (i === "Bacteria/Viruses/Fungi/Parasites") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Bacteria/Viruses/Fungi/Parasites") {
+    for (var j = 0; j < bacteria.length; j++) {
+      this.finalSelectedArray.push(bacteria[j]);
+    }
   }
-  if (i === "Bites/Stings") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Bites/Stings") {
+    for (var j = 0; j < bites.length; j++) {
+      this.finalSelectedArray.push(bites[j]);
+    }
   }
-  if (i === "Bones") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Bones") {
+    for (var j = 0; j < bones.length; j++) {
+      this.finalSelectedArray.push(bones[j]);
+    }
   }
-  if (i === "Bowels/Bladder") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Bowels/Bladder") {
+    for (var j = 0; j < bowels.length; j++) {
+      this.finalSelectedArray.push(bowels[j]);
+    }
   }
-  if (i === "Cancer") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Cancer") {
+    for (var j = 0; j < cancer.length; j++) {
+      this.finalSelectedArray.push(cancer[j]);
+    }
   }
-  if (i === "Chest/Lungs") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Chest/Lungs") {
+    for (var j = 0; j < chest.length; j++) {
+      this.finalSelectedArray.push(chest[j]);
+    }
   }
-  if (i === "Ear/Nose/Throat/Eyes") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Ear/Nose/Throat/Eyes") {
+    for (var j = 0; j < ear.length; j++) {
+      this.finalSelectedArray.push(ear[j]);
+    }
   }
-  if (i === "Fevers") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Fevers") {
+    for (var j = 0; j < fevers.length; j++) {
+      this.finalSelectedArray.push(fevers[j]);
+    }
   }
-  if (i === "General Well Being") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "General Well Being") {
+    for (var j = 0; j < general.length; j++) {
+      this.finalSelectedArray.push(general[j]);
+    }
   }
-  if (i === "Heart/Blood") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Heart/Blood") {
+    for (var j = 0; j < heart.length; j++) {
+      this.finalSelectedArray.push(heart[j]);
+    }
   }
-  if (i === "Infectious Diseases") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Infectious Diseases") {
+    for (var j = 0; j < infectious.length; j++) {
+      this.finalSelectedArray.push(infectious[j]);
+    }
   }
-  if (i === "Liver/Kidneys") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Liver/Kidneys") {
+    for (var j = 0; j < liver.length; j++) {
+      this.finalSelectedArray.push(liver[j]);
+    }
   }
-  if (i === "Nerves/Muscles") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Nerves/Muscles") {
+    for (var j = 0; j < nerves.length; j++) {
+      this.finalSelectedArray.push(nerves[j]);
+    }
   }
-  if (i === "Other") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Other") {
+    for (var j = 0; j < otherMed.length; j++) {
+      this.finalSelectedArray.push(otherMed[j]);
+    }
   }
-  if (i === "Pain Relief") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Pain Relief") {
+    for (var j = 0; j < pain.length; j++) {
+      this.finalSelectedArray.push(pain[j]);
+    }
   }
-  if (i === "Sex/Reproduction") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Sex/Reproduction") {
+    for (var j = 0; j < sex.length; j++) {
+      this.finalSelectedArray.push(sex[j]);
+    }
   }
-  if (i === "Skin/Hands/Feet") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Skin/Hands/Feet") {
+    for (var j = 0; j < skin.length; j++) {
+      this.finalSelectedArray.push(skin[j]);
+    }
   }
-  if (i === "Stomach") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Stomach") {
+    for (var j = 0; j < stomach.length; j++) {
+      this.finalSelectedArray.push(stomach[j]);
+    }
   }
-  if (i === "Wounds/Bruises") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Wounds/Bruises") {
+    for (var j = 0; j < wounds.length; j++) {
+      this.finalSelectedArray.push(wounds[j]);
+    }
   }
-  if (i === "Whole Body") {
-    //loop through, push i to finalSelectedArray
+  if (this.medicalSelected[i] === "Whole Body") {
+    for (var j = 0; j < wholeBody.length; j++) {
+      this.finalSelectedArray.push(wholeBody[j]);
+    }
   }
 }
+console.log('Line 209 ' + this.finalSelectedArray);
+return this.finalSelectedArray;
+}
 
+
+this.removeMedicalSpecific = function() {
+  var alternativeMed = ['Alternative', 'Aromatherapy', 'Bach', 'Homeopathy'];
+  var bacteria = ['Anthelmintic', 'Antibacterial', 'Antibiotic', 'Antifungal', 'Antiviral', 'Parasiticide', 'Vermifuge', 'Warts'];
+  var bites = ['Antidote', 'Stings'];
+  var bones = ['Antiarthritic', 'Antiinflammatory', 'Antirheumatic'];
+  var bowels = ['Antihaemorrhoidal', 'Aperient', 'Carminative', 'Cathartic', 'Hydrogogue', 'Laxative', 'Purgative'];
+  var cancer = ['Antitumor', 'Cancer', 'Cytostatic', 'Cytotoxic', 'Resolvent'];
+  var chest = ['Antiasthmatic', 'Antitussive', 'Decongestant', 'Demulcent', 'Expectorant', 'Pectoral'];
+  var ear = ['Errhine', 'Mouthwash', 'Mydriatic', 'Odontalgic', 'Ophthalmic', 'Sialagogue', 'Sternutatory'];
+  var fevers = ['Antipyretic', 'Febrifuge'];
+  var general = ['Antiscorbutic', 'Balsamic', 'Nutritive', 'Tonic'];
+  var heart = ['Anticholesterolemic', 'Anticoagulant', 'Blood purifier', 'Blood tonic', 'Cardiac', 'Cardiotonic', 'Haemolytic', 'Hypoglycaemic', 'Hypotensive', 'Vasoconstrictor', 'Vasodilator'];
+  var infectious = ['Antiperiodic', 'Antiscrophulatic' , 'TB', 'VD'];
+  var liver = ['Antibilious', 'Cholagogue', 'Diuretic', 'Haemostatic', 'Hepatic', 'Kidney', 'Lithontripic'];
+  var nerves = ['Antispasmodic', 'Nervine'];
+  var otherMed = ['Antidandruff', 'Antihydrotic', 'Antiphlogistic', 'Antivinous', 'Appetizer', 'Aromatic', 'Astringent', 'Bitter', 'Deodorant', 'Diaphoretic', 'Emollient', 'Enuresis', 'Hallucinogenic', 'Hypnotic', 'Irritant', 'Miscellany', 'Refrigerant', 'Restorative', 'Rubefacient',
+  'Sedative', 'Stimulant'];
+  var pain = ['Anaesthetic', 'Analgesic', 'Anodyne', 'Lenitive', 'Narcotic'];
+  var sex = ['Abortifacient', 'Anaphrodisiac', 'Aphrodisiac', 'Birthing aid', 'Contraceptive', 'Emmenagogue', 'Galactofuge', 'Galactogogue', 'Infertility', 'Oxytoxic', 'Uterine tonic', "Women's complaints"];
+  var skin = ['Acrid', 'Antidermatosic', 'Antipruritic', 'Foot care', 'Skin', 'Vesicant'];
+  var stomach = ['Antacid', 'Antiemetic', 'Digestive', 'Emetic', 'Stomachic'];
+  var wounds = ['Antiecchymotic', 'Antiseptic', 'Detergent', 'Disinfectant', 'Plaster', 'Poultice', 'Salve', 'Styptic', 'Vulnerary'];
+  var wholeBody = ['Adaptogen', 'Deobstruent', 'Depurative'];
+
+for (var i = 0; i < this.medicalSelected; i++) {
+  if (this.medicalSelected[i] === "Alternative Medicine") {
+    for (var j = 0; j < alternativeMed.length; j++) {
+    this.finalSelectedArray.splice(alternativeMed[j], 1);
+    if (this.medicalSelected[i] === "Bacteria/Viruses/Fungi/Parasites") {
+      for (var j = 0; j < bacteria.length; j++) {
+        this.finalSelectedArray.push(bacteria[j]);
+      }
+    }
+  }
+}
+  if (this.medicalSelected[i] === "Bacteria/Viruses/Fungi/Parasites") {
+    for (var j = 0; j < bacteria.length; j++) {
+      this.finalSelectedArray.splice(bacteria[j], 1);
+    }
+  }
+  }
+
+
+
+  // console.log('Line 250 ' + this.finalSelectedArray);
+  return this.finalSelectedArray;
+}
 //loop through edibleSelected
-
-for (var i = 0; i < this.edibleSelected.length; i++) {
-  if (i === "Chocolate") {
-    //loop through, push to final
-  }
-  if (i === "Coffee") {
-    //loop through, push to final
-  }
-  if (i === "Coloring") {
-    //loop through, push to final
-  }
-  if (i === "Condiment") {
-    //loop through, push to final
-  }
-  if (i === "Curdling") {
-    //loop through, push to final
-  }
-  if (i === "Drink") {
-    //loop through, push to final
-  }
-  if (i === "Egg") {
-    //loop through, push to final
-  }
-  if (i === "Gelatine") {
-    //loop through, push to final
-  }
-  if (i === "Gum") {
-    //loop through, push to final
-  }
-  if (i === "Milk") {
-    //loop through, push to final
-  }
-  if (i === "Oil") {
-    //loop through, push to final
-  }
-  if (i === "Pectin") {
-    //loop through, push to final
-  }
-  if (i === "Rutin") {
-    //loop through, push to final
-  }
-  if (i === "Salt") {
-    //loop through, push to final
-  }
-  if (i === "Stabilizer") {
-    //loop through, push to final
-  }
-  if (i === "Sweetener") {
-    //loop through, push to final
-  }
-  if (i === "Tea") {
-    //loop through, push to final
-  }
-}
-
-//loop through otherSelected
-for (var i = 0; i < this.otherSelected.length; i++) {
-  if (i === "Building") {
-    //loop through, push to final
-  }
-  if (i === "Clothing") {
-    //loop through, push to final
-  }
-  if (i === "Dyes/Paints/Ink/Paper") {
-    //loop through, push to final
-  }
-  if (i === "Fertilizers") {
-    //loop through, push to final
-  }
-  if (i === "Fire/Lightning") {
-    //loop through, push to final
-  }
-  if (i === "Bathroom") {
-    //loop through, push to final
-  }
-  if (i === "Garden") {
-    //loop through, push to final
-  }
-  if (i === "Home") {
-    //loop through, push to final
-  }
-  if (i === "Kitchen") {
-    //loop through, push to final
-  }
-  if (i === "Other") {
-    //loop through, push to final
-  }
-  if (i === "Pesticides") {
-    //loop through, push to final
-  }
-  if (i === "Woodwork/Crafts") {
-    //loop through, push to final
-  }
-}
+//
+// for (var i = 0; i < this.edibleSelected.length; i++) {
+//   if (i === "Chocolate") {
+//     //loop through, push to final
+//   }
+//   if (i === "Coffee") {
+//     //loop through, push to final
+//   }
+//   if (i === "Coloring") {
+//     //loop through, push to final
+//   }
+//   if (i === "Condiment") {
+//     //loop through, push to final
+//   }
+//   if (i === "Curdling") {
+//     //loop through, push to final
+//   }
+//   if (i === "Drink") {
+//     //loop through, push to final
+//   }
+//   if (i === "Egg") {
+//     //loop through, push to final
+//   }
+//   if (i === "Gelatine") {
+//     //loop through, push to final
+//   }
+//   if (i === "Gum") {
+//     //loop through, push to final
+//   }
+//   if (i === "Milk") {
+//     //loop through, push to final
+//   }
+//   if (i === "Oil") {
+//     //loop through, push to final
+//   }
+//   if (i === "Pectin") {
+//     //loop through, push to final
+//   }
+//   if (i === "Rutin") {
+//     //loop through, push to final
+//   }
+//   if (i === "Salt") {
+//     //loop through, push to final
+//   }
+//   if (i === "Stabilizer") {
+//     //loop through, push to final
+//   }
+//   if (i === "Sweetener") {
+//     //loop through, push to final
+//   }
+//   if (i === "Tea") {
+//     //loop through, push to final
+//   }
+// }
+//
+// //loop through otherSelected
+// for (var i = 0; i < this.otherSelected.length; i++) {
+//   if (i === "Building") {
+//     //loop through, push to final
+//   }
+//   if (i === "Clothing") {
+//     //loop through, push to final
+//   }
+//   if (i === "Dyes/Paints/Ink/Paper") {
+//     //loop through, push to final
+//   }
+//   if (i === "Fertilizers") {
+//     //loop through, push to final
+//   }
+//   if (i === "Fire/Lightning") {
+//     //loop through, push to final
+//   }
+//   if (i === "Bathroom") {
+//     //loop through, push to final
+//   }
+//   if (i === "Garden") {
+//     //loop through, push to final
+//   }
+//   if (i === "Home") {
+//     //loop through, push to final
+//   }
+//   if (i === "Kitchen") {
+//     //loop through, push to final
+//   }
+//   if (i === "Other") {
+//     //loop through, push to final
+//   }
+//   if (i === "Pesticides") {
+//     //loop through, push to final
+//   }
+//   if (i === "Woodwork/Crafts") {
+//     //loop through, push to final
+//   }
+// }
 
 
 })

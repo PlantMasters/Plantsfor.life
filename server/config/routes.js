244837d40e014f6e98121238596b3cad7071412f
Jessica Morrell <jessica.ann.morrell@gmail.com>
2016-04-15 16:16:53 -0600

    fixed limit

--- server/config/routes.js
+++ server/config/routes.js
@@ -37,34 +37,34 @@ module.exports = function(app) {
       
       if (req.body.zone) {
         
-        Plants.find({$or: [ {$and: [{'uses.edible': {$in: req.body.edible}}, {'zone': req.body.zone}]}, {$and: [{'uses.medical': {$in: req.body.medical}}, {'zone': req.body.zone}]}, {$and: [{'uses.other': {$in: req.body.other}}, {'zone': req.body.zone}]}]}, function(err0, plants) {
-            if (err0) {
-                res.status(500).send(err0);
-            } else {
-                plantsArray.push(plants);
-                console.log(plantsArray);
-                console.log(plantsArray.length);
-                res.send(plantsArray);
-            }
-            
-        })
+            Plants.find({$or: [ {$and: [{'uses.edible': {$in: req.body.edible}}, {'zone': req.body.zone}]}, {$and: [{'uses.medical': {$in: req.body.medical}}, {'zone': req.body.zone}]}, {$and: [{'uses.other': {$in: req.body.other}}, {'zone': req.body.zone}]}]}, {}, {limit: 50}, function(err0, plants) {
+                if (err0) {
+                    res.status(500).send(err0);
+                } else {
+                    plantsArray.push(plants);
+                    console.log(plantsArray);
+                    console.log(plantsArray.length);
+                    res.send(plantsArray);
+                }
+                
+            })
         //console.log(plantsArray);
          //res.send(plantsArray);
       } 
       else {
-          Plants.find({$or: [ {'uses.edible': {$in: req.body.edible}}, {'uses.medical': {$in: req.body.medical}}, {'uses.other': {$in: req.body.other}}]}, function(err0, plants) {
-            if (err0) {
-                res.status(500).send(err0);
-            } else {
-                plantsArray.push(plants);
-                console.log(plantsArray);
-                console.log(plantsArray.length);
-                res.send(plantsArray);
-            }
-            
-        })
+            Plants.find({$or: [ {'uses.edible': {$in: req.body.edible}}, {'uses.medical': {$in: req.body.medical}}, {'uses.other': {$in: req.body.other}}]}, {}, {limit: 50}, function(err0, plants) {
+                if (err0) {
+                    res.status(500).send(err0);
+                } else {
+                    plantsArray.push(plants);
+                    console.log(plantsArray);
+                    console.log(plantsArray.length);
+                    res.send(plantsArray);
+                }
+                
+            })
           
-       }
+      }
   }) 
       
     

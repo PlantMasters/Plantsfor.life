a72247382423ef81c56b06f0c1253bca54369e34
Jessica Hathaway <hathaway.jess@gmail.com>
2016-04-18 11:43:59 -0600

    modals

--- main/js/controllers/ModalCtrl.js
+++ main/js/controllers/ModalCtrl.js
@@ -2,11 +2,20 @@ angular.module('plantMasters')
 
 .controller('ModalCtrl', ['$scope', 'ModalService', function($scope, ModalService) {
 
-  $scope.showCustom = function() {
+  $scope.showCustom = function(plant) {
 
     ModalService.showModal({
       templateUrl: "../views/plant-modal.html",
-      controller: "ModalController"
+      controller: "ModalController",
+      plant: plant,
+      resolve: {
+        plants: function() {
+          return $scope.plants;
+        },
+        plant: function() {
+          return plant;
+        }
+      }
     }).then(function(modal) {
       modal.close.then(function(result) {
         $scope.customResult = "All good!";
@@ -16,8 +25,10 @@ angular.module('plantMasters')
   };
 }])
 
-.controller('ModalController', ['$scope', 'close', function($scope, close) {
+.controller('ModalController', ['$scope', 'close', function($scope, close, plants, plant) {
 
   $scope.close = close;
+  $scope.plants = plants;
+  $scope.plant = plant;
 
 }]);

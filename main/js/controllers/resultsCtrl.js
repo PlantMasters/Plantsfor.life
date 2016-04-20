0126fb7dcedfa2fb0803de66087ce1087cbfd239
Jessica Hathaway <hathaway.jess@gmail.com>
2016-04-20 16:35:10 -0600

    images on results page

--- main/js/controllers/resultsCtrl.js
+++ main/js/controllers/resultsCtrl.js
@@ -48,3 +48,14 @@ angular.module('plantMasters').filter('range', function() {
     return val;
   };
 });
+
+angular.module('plantMasters').directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : 'cover'
+            });
+        });
+    };
+});

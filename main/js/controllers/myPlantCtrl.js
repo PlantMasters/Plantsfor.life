5198294caf4f58b89d1d3860ddc8e193762370c6
Jessica Morrell <jessica.ann.morrell@gmail.com>
2016-04-21 17:02:15 -0600

    working on editing inputs and other css on myPlant page

--- main/js/controllers/myPlantCtrl.js
+++ main/js/controllers/myPlantCtrl.js
@@ -2,7 +2,13 @@ angular.module('plantMasters').controller('myPlantCtrl', function($scope, plantR
     $scope.myPlant = plantRef;
     console.log("MY PLANT")
     console.log($scope.myPlant);
-    $scope.myPlantDate = "Enter date planted...";
+    
+    $scope.myPlantDate;
+    $scope.myPlantVariety;
+    $scope.myPlantLocation;
+    $scope.myPlantNotes;
+    
+    
     $scope.updateDate = function(newDate) {
         console.log('NEW DATE');
         console.log(newDate);
@@ -18,6 +24,10 @@ angular.module('plantMasters').controller('myPlantCtrl', function($scope, plantR
         return $scope.inputBoolean;
     }
     
+    
+    $scope.updateVariety = function(newVar) {
+        $scope.myPlantVariety = newVar;
+    }
     $scope.varietyBoolean = true;
     $scope.hideVarietyInput = function() {
         $scope.varietyBoolean = true;
@@ -26,5 +36,29 @@ angular.module('plantMasters').controller('myPlantCtrl', function($scope, plantR
     $scope.showVarietyInput = function() {
         $scope.varietyBoolean = false;
         return $scope.varietyBoolean;
+    }
+    
+    
+    $scope.updateLocation = function(newLoc) {
+        $scope.myPlantLocation = newLoc;
+    }
+    $scope.locationBoolean = true;
+    $scope.hideLocationInput = function() {
+        $scope.locationBoolean = true;
+    }
+    $scope.showLocationInput = function() {
+        $scope.locationBoolean = false;
+    }
+    
+    
+    $scope.updateNotes = function(newNote) {
+        $scope.myPlantNotes = newNote;
+    }
+    $scope.notesBoolean = true;
+    $scope.hideNotesInput = function() {
+        $scope.notesBoolean = true;
+    }
+    $scope.showNotesInput = function() {
+        $scope.notesBoolean = false;
     }
 });

1bb6fe1469eec65dac5dbe7fe0411b31598cbee9
Tom Pridham <pridham.tom@gmail.com>
2016-04-22 14:13:44 -0600

    added about page

--- main/js/app.js
+++ main/js/app.js
@@ -1,50 +1,53 @@
 angular.module('plantMasters', ['ngResource', 'ui.router', 'angularModalService', 'ngAnimate'])
 
 
-
-.config(function($urlRouterProvider, $stateProvider) {
-  var routeRoleChecks = {
-    user: {
-      auth: function(Auth) {
-        return Auth.authorizeAuthenticatedUserForRoute()
-      }
-    }
-  };
-  $stateProvider
-    .state('main', {
-      url: '/',
-      views: {
-        'search': {
-          templateUrl: 'views/main.html',
-          controller: 'mainCtrl'
-        },
-        'results': {
-          templateUrl: 'views/results.html',
-          controller: 'resultsCtrl'
-        }
-      }
-    })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: 'views/signup.html',
-      controller: 'SignupCtrl'
-    })
-    .state('profile', {
-      url: '/profile',
-      templateUrl: 'views/profile.html',
-      controller: 'ProfileCtrl',
-      resolve: routeRoleChecks.user
-    })
-    .state('myGarden', {
-        url: '/myGarden',
-        templateUrl: 'views/myGarden.html',
-        controller: 'myGardenCtrl',
-        resolve: {
-            gardenRef: function(gardenService) {
-                return gardenService.getGarden();
+    .config(function ($urlRouterProvider, $stateProvider) {
+        var routeRoleChecks = {
+            user: {
+                auth: function (Auth) {
+                    return Auth.authorizeAuthenticatedUserForRoute()
+                }
             }
-        }
-    })
-
-  $urlRouterProvider.otherwise('/');
-});
+        };
+        $stateProvider
+            .state('main', {
+                url: '/',
+                views: {
+                    'search': {
+                        templateUrl: 'views/main.html',
+                        controller: 'mainCtrl'
+                    },
+                    'results': {
+                        templateUrl: 'views/results.html',
+                        controller: 'resultsCtrl'
+                    }
+                }
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'views/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'views/profile.html',
+                controller: 'ProfileCtrl',
+                resolve: routeRoleChecks.user
+            })
+            .state('myGarden', {
+                url: '/myGarden',
+                templateUrl: 'views/myGarden.html',
+                controller: 'myGardenCtrl',
+                resolve: {
+                    gardenRef: function (gardenService) {
+                        return gardenService.getGarden();
+                    }
+                }
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            });
+        $urlRouterProvider.otherwise('/');
+    });

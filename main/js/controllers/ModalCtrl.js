cf1f0062baae732bbc337558af7eaba9419810a1
David Andelin <iandelin@outlook.com>
2016-04-26 16:56:26 -0600

    working on the tooltip

--- main/js/controllers/ModalCtrl.js
+++ main/js/controllers/ModalCtrl.js
@@ -1,9 +1,14 @@
 angular.module('plantMasters')
 
-.controller('ModalCtrl', ['$scope', 'close', 'plant', function($scope, close, plant) {
+.controller('ModalCtrl', ['$scope', 'close', 'plant', function($http, $scope, close, plant) {
 
   $scope.plant = plant;
   $scope.close = close;
 
-
+  $scope.toolCall = function() {
+    $http.get('../medCats.json').then(function(response) {
+      var definition = response.data;
+      console.log(definition);
+    });
+  };
 }]);

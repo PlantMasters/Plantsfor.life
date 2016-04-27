5a81a8a0efad5aaf7e2c392c8742548b43b9ab6d
David Andelin <iandelin@outlook.com>
2016-04-27 17:28:10 -0600

    added gulp nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-//var uglify = require('gulp-uglify')
+var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var ngAnnotate = require('gulp-ng-annotate');
 var server = require('gulp-webserver');
 var htmlmin = require('gulp-htmlmin');
 var babel = require('gulp-babel');
-
+var nodemon = require('gulp-nodemon');
 
 var watcher = gulp.watch(['./main/js/**/*.js', './main/styles/*.scss', './main/styles/*.css', './main/views/**/**/*.html', './main/*.html'], ['default']);
 watcher.on('change', function( event ) {
@@ -19,31 +19,38 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(uglifycss())
         .pipe(concat('styles.css'))
-        .pipe(gulp.dest('./public/styles'))
+        .pipe(gulp.dest('./public/styles'));
 });
 gulp.task('css', function() {
   gulp.src('./main/styles/*.css')
   .pipe(uglifycss())
-  .pipe(gulp.dest('./public/styles'))
+  .pipe(gulp.dest('./public/styles'));
 });
 gulp.task('javascript', function() {
     gulp.src('./main/js/**/*.js')
-        //.pipe(ngAnnotate())
+        .pipe(ngAnnotate())
         .pipe(babel({
             presets: ['es2015']
         }))
-        // .pipe(uglify())
+        .pipe(uglify())
         .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('./public/scripts'))
+        .pipe(gulp.dest('./public/scripts'));
 });
 gulp.task('html', function() {
     gulp.src('./main/views/**/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./public/views'))
+        .pipe(gulp.dest('./public/views'));
 });
 gulp.task('index', function() {
     gulp.src('./main/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./public'));
 });
+gulp.task('run', function() {
+  nodemon({
+    script: 'server.js'
+  , ext: 'js html'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
 gulp.task('default', ['sass', 'css', 'javascript', 'html', 'index']);

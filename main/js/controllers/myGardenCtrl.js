22d27fe285e82b19462b296e2f2d2718bb47f7b2
Jessica Hathaway <hathaway.jess@gmail.com>
2016-04-28 10:48:23 -0600

    final commit

--- main/js/controllers/myGardenCtrl.js
+++ main/js/controllers/myGardenCtrl.js
@@ -1,7 +1,7 @@
 angular.module('plantMasters').controller('myGardenCtrl', function($scope, gardenRef, gardenService) {
     $scope.myGarden = gardenRef.data
     $scope.removePlant = function(plant) {
-        var c = confirm('Are you sure you want to remove this plant from your garden? This will perminently remove it from the database and it will be gone forever!');
+        var c = confirm('Are you sure you want to remove this plant from your garden?');
         if(c === true) {
             gardenService.deletePlant(plant)
             .then(function(response) {

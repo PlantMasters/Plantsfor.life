ee84447ca5bbe0c62e077ffdd48c560eb7fdc5b5
David Andelin <iandelin@outlook.com>
2016-09-30 12:22:52 -0600

    added aggregate sample results

--- server/config/results.js
+++ server/config/results.js
@@ -61,17 +61,17 @@ module.exports = {
         }
     },
     samplePlants: (req, res)=> {
-        // Plants.aggregate(
-        //     {$sample: {size: 24}}, (err, plants)=> {
-        //         err ? res.status(500).send(err) : res.send(plants);
-        //     }
-        // );
-        Plants.find().sort({views:-1}).limit(24).select({}).exec((err0, plants)=>{
-          if (err0) {
-              res.status(500).send(err0);
-          } else {
-              res.send(plants);
-          }});
+        Plants.aggregate(
+            {$sample: {size: 24}}, (err, plants)=> {
+                err ? res.status(500).send(err) : res.send(plants);
+            }
+        );
+        // Plants.find().sort({views:-1}).limit(24).select({}).exec((err0, plants)=>{
+        //   if (err0) {
+        //       res.status(500).send(err0);
+        //   } else {
+        //       res.send(plants);
+        //   }});
       },
     //This searches for plants that match the string input into the input field...
     searchResults: (req, res)=> {
